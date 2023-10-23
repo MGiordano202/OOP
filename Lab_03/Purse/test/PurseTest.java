1f2d13e90498d67bb1b56af43e3dd712dcbcb225
MGiordano202 <114730112+MGiordano202@users.noreply.github.com>
2023-10-23 17:11:39 +0200

    Purse-lab3 completato nacc

--- Lab_03/Purse/test/PurseTest.java
+++ Lab_03/Purse/test/PurseTest.java
@@ -5,41 +5,100 @@ public class PurseTest {
 
     @Test
     public void findTest() {
-    /* TODO */
+    Purse p = new Purse();
+
+    p.add(Purse.QUARTER);
+    Assert.assertTrue(p.find(Purse.QUARTER));
+    Assert.assertFalse(p.find(Purse.DIME));
+
     }
 
     @Test
     public void countTest() {
-    /* TODO */
+    Purse p = new Purse();
+
+    p.add(Purse.DOLLAR);
+
+    Assert.assertEquals(1,p.count(Purse.DOLLAR),0);
+    Assert.assertEquals(0, p.count(Purse.CENT));
     }
 
     @Test
     public void minMaxTest() {
-    /* TODO */
+
+        Purse p = new Purse();
+        p.add(Purse.DOLLAR);
+        p.add(Purse.DIME);
+        Assert.assertEquals(Purse.DOLLAR, p.getMaximum());
+        Assert.assertEquals(Purse.DIME, p.getMinimum());
+
     }
 
     @Test
-    public void getTotalTest() {
-    /* TODO */
+    public void getTotalTest()
+    {
+
+        Purse p = new Purse();
+        p.add(Purse.DOLLAR);
+        p.add(Purse.QUARTER);
+        p.add(Purse.DIME);
+
+        Assert.assertEquals(1.35, p.getTotal(),0);
+
     }
 
     @Test
     public void addRemoveTest() {
-    /* TODO */
+
+        Purse p = new Purse();
+
+        p.add(Purse.CENT);
+        Assert.assertEquals(1, p.count(Purse.CENT));
+
+        p.remove(Purse.CENT);
+        Assert.assertEquals(0, p.count(Purse.CENT));
+
+
     }
 
     @Test
     public void hasCoinTest() {
-    /* TODO */
+
+        Purse p = new Purse();
+
+        p.add(Purse.DOLLAR);
+        Assert.assertTrue(p.hasCoin(Purse.DOLLAR));
+        Assert.assertFalse(p.hasCoin(Purse.DIME));
+
     }
 
     @Test
     public void toStringTest() {
-    /* TODO */
+        Purse p = new Purse();
+        p.add(Purse.DOLLAR);
+        p.add(Purse.CENT);
+        p.add(Purse.QUARTER);
+
+        Assert.assertEquals("Purse[Dollar = 1, Quarter = 1, Dime = 0, Nickel = 0, Cent = 1]", p.toString());
+
+
+
     }
 
     @Test
     public void equalsTest() {
-    /* TODO */
+        Purse p1 = new Purse();
+        p1.add(Purse.DOLLAR);
+        p1.add(Purse.QUARTER);
+
+        Purse p2 = new Purse();
+        p2.add(Purse.DOLLAR);
+        p2.add(Purse.QUARTER);
+        Assert.assertEquals(p1, p2);
+
+        Purse p3 = new Purse();
+        p3.add(Purse.CENT);
+
+        Assert.assertNotEquals(p2,p3);
     }
 }
